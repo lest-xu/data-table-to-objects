3f7c0a6415249cdf0ea4ea5e4c99f9134e9bb9a8
autodataxx <34578322+autodataxx@users.noreply.github.com>
2018-04-06 11:54:36 -0400

    Update Person.cs

--- Person.cs
+++ Person.cs
@@ -1,1 +1,15 @@
+using System;
+using System.ComponentModel;
+using System.Data;
 
+namespace example.Models
+{
+    public class Person
+    {
+      public int ID { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public char Sex { get; set; }
+      public DateTime DateofBirth { get; set; }
+    }
+}
