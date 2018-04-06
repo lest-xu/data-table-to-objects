855e4f9324c04ccf7a6b36bef2df8d5abe2a6520
autodataxx <34578322+autodataxx@users.noreply.github.com>
2018-04-06 13:07:55 -0400

    Update Select.cs

--- Select.cs
+++ Select.cs
@@ -1,1 +1,23 @@
+public DataTable Select(string storedProcedureorCommandText, bool isStoredProcedure = true)
+{
+    DataTable dataTable = new DataTable();
+    using (SqlConnection connection = new SqlConnection("ConnectionString"))
+    {
+        using (SqlCommand command = new SqlCommand())
+        {
+            command.Connection = connection;
+            command.CommandType = CommandType.StoredProcedure;
+            if (!isStoredProcedure)
+            {
+            command.CommandType = CommandType.Text;
+            }
+            command.CommandText = storedProcedureorCommandText;
+            connection.Open();
 
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            dataAdapter.Fill(dataTable);
+
+            return dataTable;
+        }
+    }
+}
